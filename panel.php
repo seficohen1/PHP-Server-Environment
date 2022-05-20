1c7a46cfb0bbdab87cf9271093baf8fc86009769
Sefi Cohen <seficohen1@gmail.com>
2022-05-20 15:43:40 +0200

    :passport_control: updated validate and panel files
    added validation and starting session

---
+++ panel.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Panel</title>
+  <style>
+    span {
+      color: blue;
+      font-weight: bold;
+    }
+  </style>
+</head>
+<body>
+  <h1>Hello <span>Sefi</span>, you are now logged in to your panel</h1>
+  <p>you're email is <span>email</span></p>
+  <button>Log out</button>
+</body>
+</html>
---
+++ validate.php
@@ -0,0 +1,26 @@
+<?php
+
+// mock database for this exercise
+$dbusername = 'seficohen1';
+$dbemail = 'seficohen1@gmail.com';
+$dbpassword = '123456';
+$dbpasswordEnc = password_hash($dbpassword, PASSWORD_DEFAULT); // PASSWRORD_DEFAULT is considerd best practice to use rather than BCRYPT
+
+// checking if recieved input fields corespond with database
+
+$username = $_POST['user'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+if($dbusername === $username AND $dbemail === $email AND password_verify($password, $dbpasswordEnc)) 
+{
+  session_start();
+  $_SESSION['user'] = $username;
+  $_SESSION['email'] = $email;
+  header('Location: panel.php');
+
+} else {
+  echo "you can't log in";
+}
+
